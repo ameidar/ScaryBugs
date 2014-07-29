dad326e759fbd3e026ac0d36236cc1c4b93c0896
ami meidar <ami.meidar@gmail.com>
2014-07-29 17:18:06 +0300

    add files

---
+++ ScaryBugs/RWTScaryBugData.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+@interface RWTScaryBugData : NSObject
+
+@property (strong) NSString *title;
+@property (assign) float rating;
+
+- (id)initWithTitle:(NSString*)title rating:(float)rating;
+
+@end
