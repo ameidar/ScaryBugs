9cebb6ab71eaf617fb26ca9adf893e04d626a675
igrois <inna@Innas-Mac-mini.local>
2014-08-04 23:20:55 +0200

    Categories view controller is added

---
+++ ScaryBugs/RWTCategoriesViewController.h
@@ -0,0 +1,15 @@
+//
+//  RWTCategoriesViewController.h
+//  ScaryBugs
+//
+//  Created by Inna Grois on 7/26/14.
+//  Copyright (c) 2014 Inna Grois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWTCategoriesViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong) NSMutableArray *categories ;
+
+@end
